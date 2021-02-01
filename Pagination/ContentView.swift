202cc3f2d15080b97fd599036dad63a1c28456a5
Philip Davis <philipcdavis11@gmail.com>
2021-02-01 11:51:48 -0500

    Added pagination example

--- Pagination/ContentView.swift
+++ Pagination/ContentView.swift
@@ -9,10 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.blue.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            TabView {
+                ForEach(0..<30) { i in
+                    ZStack {
+                        Color.black.opacity(0.4)
+                        Text("Page \(i)").foregroundColor(.white)
+                        
+                    }.cornerRadius(12)
+                    
+                }.padding(.all, 10)
+            }
+            .frame(width: UIScreen.main.bounds.width, height: 400)
+            .tabViewStyle(PageTabViewStyle())
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
