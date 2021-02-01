a09c39c2adde5038427308ead719d119a0d9f602
Philip Davis <philipcdavis11@gmail.com>
2021-02-01 11:44:11 -0500

    Initial Commit

---
+++ Pagination/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pagination
+//
+//  Created by Philip Davis on 2/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
